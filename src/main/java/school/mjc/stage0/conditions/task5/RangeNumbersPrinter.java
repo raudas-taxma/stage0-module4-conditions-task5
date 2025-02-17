3914e54ea3379317b80e76d0c52a5720dabd1dc9
rgsad <rsad3262@gmail.com>
2025-02-17 10:30:49 +0300

    solution

--- src/main/java/school/mjc/stage0/conditions/task5/RangeNumbersPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/RangeNumbersPrinter.java
@@ -2,5 +2,26 @@ package school.mjc.stage0.conditions.task5;
 
 public class RangeNumbersPrinter {
     public void printRange(int number) {
+        String range = "";
+        switch (number) {
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+                range = "number is between 1 and 5";
+                break;
+            case 6:
+            case 7:
+            case 8:
+            case 9:
+            case 10:
+                range = "number is between 6 and 10";
+                break;
+            default:
+                range = "out of range";
+                break;
+        }
+        System.out.println(range);
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task5/SwitchDaysInMonthPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/SwitchDaysInMonthPrinter.java
@@ -2,5 +2,21 @@ package school.mjc.stage0.conditions.task5;
 
 public class SwitchDaysInMonthPrinter {
     public void amountOfDays(int month) {
+        String days = "31";
+        switch (month) {
+            case 2:
+                days = "28";
+                break;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                days = "30";
+                break;
+            default:
+                days = "wrong number!";
+                break;
+        }
+        System.out.println(days);
     }
 }
